2063e6fa3062146488f97933e9d1347b7ef53fda
Jessie Huff <huffjd1@gmail.com>
2018-02-07 19:22:26 +0000

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,16 @@
 class CartsController < ApplicationController
+  def show
+    @current_cart = Cart.find_by(:id => params[:id])
+  end
 
+  def checkout
+    @current_cart = Cart.find(params[:id])
+    @current_cart.items.each.with_index do |item, i|
+      item.inventory -= @current_cart.line_items[i].quantity
+      item.save
+    end
+    @current_cart.update(status: "Submitted")
+    current_user.update(current_cart: nil)
+    redirect_to cart_path(@current_cart)
+  end
 end
